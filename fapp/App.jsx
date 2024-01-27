738dd0725c5f50db13c3244757b42f276a0f240e
SHAFIQUDDIN SHAIKH <shafiquddinshaikh9@gmail.com>
2024-01-27 11:12:44 +0530

    app initialized

---
+++ fapp/App.jsx
@@ -0,0 +1,24 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '800',
+  },
+});
